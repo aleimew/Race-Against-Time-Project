ca71a1854968ef87709555f7768d0bf5943e4165
Joshua <cesteraime@gmail.com>
2026-01-04 14:48:04 -0800

    Save local work before merging master

--- Assets/_Core/Scripts/GoalZone/GoalZone.cs
+++ Assets/_Core/Scripts/GoalZone/GoalZone.cs
@@ -5,12 +5,36 @@ using UnityEngine.SceneManagement;
 
 public class GoalZone : MonoBehaviour
 {
-    [SerializeField] private ScoreBoard refScoreBoard;
+    private double timer = 0;
+    private bool timerIsRunning = false;
+
+    private void NextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    private void Update()
+    {
+        if (timerIsRunning)
+        {
+            timer += Time.deltaTime;
+            if (timer >= 3)
+            {
+                NextScene();
+            }
+        }
+    }
+
+    private void TimerStart()
+    {
+        timerIsRunning = true;
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        refScoreBoard.PostScore();
-
+        ScoreBoard.Instance.PostScore();
+        GetComponent<Collider2D>().enabled = false;
+        TimerStart();
         //load the next level after letting the player look at their time
         //SceneManager.LoadScene("scene");
     }
--- Assets/_Core/Scripts/ScoreBoard/ScoreBoard.cs
+++ Assets/_Core/Scripts/ScoreBoard/ScoreBoard.cs
@@ -2,29 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class ScoreBoard : MonoBehaviour
 {
     private float startTime;
-    [SerializeField] TextMeshProUGUI text;
+    //[SerializeField] TextMeshProUGUI text;
+
+    public static ScoreBoard Instance;
+    public UnityEvent onLevelCompleted;
+
+    public bool levelCompleted { get; private set; } = false;
+    public string levelCompletionText { get; private set; }
+
+    float currentTime = 0;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
 
     void Start()
     {
-        startTime = Time.time;
+        //startTime = Time.time;
     }
 
     private void Update()
     {
+        currentTime += Time.deltaTime;
         //text.text = "Time: " + (Time.time - startTime).ToString("F2");
     }
 
     public float GetTime()
     {
-        return Time.time - startTime;
+        return currentTime;
+        //return Time.time - startTime;
     }
 
     public void PostScore()
     {
-        text.text = "You Win! -- It Took You " + GetTime() + " Seconds!";
+        //text.text = "You Win! -- It Took You " + GetTime() + " Seconds!";
+        levelCompletionText = $"You Win! -- It Took You {GetTime():F2} Seconds!";
+        levelCompleted = true;
+        onLevelCompleted?.Invoke();
     }
 }
